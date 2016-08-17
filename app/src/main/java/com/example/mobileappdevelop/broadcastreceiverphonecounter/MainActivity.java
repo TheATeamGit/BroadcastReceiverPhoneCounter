6c1383650717d5f644b111faed14277afb30b0e0
TheATeamGit <theateamgit@gmail.com>
2016-08-17 17:24:52 +0600

    gjghhjg

--- app/src/main/java/com/example/mobileappdevelop/broadcastreceiverphonecounter/MainActivity.java
+++ app/src/main/java/com/example/mobileappdevelop/broadcastreceiverphonecounter/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.mobileappdevelop.broadcastreceiverphonecounter;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+    String phoneNumber;
+    private static final String ACCESS_FILE="Accessability";
+    private static String PHONE_NUMBER;
+    private static final String PASS_WORD="pass";
+    private SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    Context context;
+    int anInt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferences=getSharedPreferences(ACCESS_FILE,Context.MODE_PRIVATE);
+        editor=sharedPreferences.edit();
     }
 }
--- app/src/main/java/com/example/mobileappdevelop/broadcastreceiverphonecounter/MyReceiver.java
+++ app/src/main/java/com/example/mobileappdevelop/broadcastreceiverphonecounter/MyReceiver.java
@@ -5,15 +5,25 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.provider.Settings;
 import android.telecom.TelecomManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
     String phoneNumber;
-
+    private static final String ACCESS_FILE="Accessability";
+    private static String PHONE_NUMBER;
+    private static final String PASS_WORD="pass";
+    private SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    Context context;
+    int anInt;
     public MyReceiver() {
+
+
     }
 
     @Override
@@ -21,24 +31,66 @@ public class MyReceiver extends BroadcastReceiver {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
 
+        this.context=context;
+        sharedPreferences=this.context.getSharedPreferences(ACCESS_FILE,Context.MODE_PRIVATE);
+        editor=sharedPreferences.edit();
         if(intent.getAction().equals (Intent. ACTION_NEW_OUTGOING_CALL))
         {
-            phoneNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+            PHONE_NUMBER = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
 
+            if(!sharedPreferences.contains("PHONE_NUMBER"))
+            {
+                Log.e("This is null",PHONE_NUMBER);
+                //Toast.makeText(context,"This is null",Toast.LENGTH_LONG);
 
+                editor.putInt(PHONE_NUMBER,0);
+                editor.commit();
+            }
+
+            anInt = sharedPreferences.getInt(PHONE_NUMBER,-1);
+            anInt=anInt+1;
+            editor.putInt(PHONE_NUMBER,anInt);
+
+            PHONE_NUMBER= PHONE_NUMBER+"("+anInt+")";
             Notification notification = new Notification.Builder(context)
                     .setSmallIcon(R.mipmap.ic_launcher)
                     .setContentTitle("Outgoing Number")
-                    .setContentText(phoneNumber)
+                    .setContentText(PHONE_NUMBER)
                     .setAutoCancel(true)
                     .build();
             NotificationManager manager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
             manager.notify((int) System.currentTimeMillis(),notification);
-
+            PHONE_NUMBER=null;
         }
+
+
         else if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_IDLE))
         {
 
+            /*if(sharedPreferences.getInt(PHONE_NUMBER,-1)==-1)
+            {
+                this.context=context;
+                Log.e("This is null","This is null");
+                //Toast.makeText(context,"This is null",Toast.LENGTH_LONG);
+
+                editor.putInt(PHONE_NUMBER,0);
+                editor.commit();
+            }
+
+                anInt = sharedPreferences.getInt(PHONE_NUMBER,-1);
+                anInt=anInt+1;
+                editor.putInt(PHONE_NUMBER,anInt);
+
+            PHONE_NUMBER= PHONE_NUMBER+"("+anInt+")";
+            Notification notification = new Notification.Builder(context)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("Outgoing Number")
+                    .setContentText(PHONE_NUMBER)
+                    .setAutoCancel(true)
+                    .build();
+            NotificationManager manager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify((int) System.currentTimeMillis(),notification);
+            PHONE_NUMBER=null;*/
         }
     }
 }
